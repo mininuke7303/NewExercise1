9c9ecfbc5dfa2cc4f065d2dd06d0ad61f8cba426
KwangSoo Yoo <kwangsoo.yoo@gmail.com>
2020-03-19 09:10:15 +0900

    주석 추가함

--- WebApplication1/Startup.cs
+++ WebApplication1/Startup.cs
@@ -12,6 +12,13 @@ namespace WebApplication1 {
     public class Startup {
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+
+
+
+            // This is my TEST 광수유 하하호호하하호호
+
+
+
         public void ConfigureServices(IServiceCollection services) {
         }
 
